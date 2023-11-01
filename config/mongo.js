77e3db05d62dc02aef2f2efe757dd65d59afcf06
Rjchi <richardospina18@gmail.com>
2023-11-01 16:26:06 -0500

    [Feat] Agregar configuración de mongoose para establecer la conexión a traves de la URI

---
+++ config/mongo.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+  /**-----------------------------------------------------------------------------------------
+   * | La URI(Identificador Uniforme de Recurso) de conexión
+   * | es conocida tambien como cadena de conexión
+   * -----------------------------------------------------------------------------------------*/
+  const DB_URI = process.env.DB_URI;
+
+  await mongoose
+    .connect(DB_URI)
+    .then(() => console.log("****Conexión Establecida****"))
+    .catch((error) =>
+      console.log("****Conexión No Establecida****", error.message)
+    );
+};
+
+module.exports = dbConnect;
--- index.js
+++ index.js
@@ -2,10 +2,16 @@ require("dotenv").config();
 
 const cors = require("cors");
 const express = require("express");
+const dbConnect = require("./config/mongo");
 
 const PORT = process.env.PORT || 5001;
+
 const app = express();
 
 app.use(cors());
 
 app.listen(PORT, () => console.log(`Server on port: ${PORT}`));
+
+(async () => {
+  await dbConnect();
+})();
