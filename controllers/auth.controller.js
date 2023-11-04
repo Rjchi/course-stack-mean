5ca0f7a9c232a3aff25a35a34b84b597c3067f3f
Rjchi <richardospina18@gmail.com>
2023-11-04 14:15:28 -0500

    [Feat] Agregar controlador de logueo de un usuario

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,26 +4,64 @@ const { usersModel } = require("../models");
 const { tokenSing } = require("../utils/handleJwt");
 const { compare, encrypt } = require("../utils/handlePassword");
 
+const handleErrors = require("../utils/handleError");
+
 const register = async (req, res) => {
-  req = matchedData(req);
+  try {
+    req = matchedData(req);
 
-  /**--------------------------------------
-   * | Generamos la contraseña encriptada
-   * --------------------------------------*/
-  const password = await encrypt(req.password);
-  const body = { ...req, password };
+    /**--------------------------------------
+     * | Generamos la contraseña encriptada
+     * --------------------------------------*/
+    const password = await encrypt(req.password);
+    const body = { ...req, password };
 
-  const dataUser = await usersModel.create(body);
-  dataUser.set("password", undefined, { strict: false });
+    const dataUser = await usersModel.create(body);
+    dataUser.set("password", undefined, { strict: false });
 
-  const data = {
-    token: await tokenSing(dataUser),
-    user: dataUser,
-  };
+    const data = {
+      token: await tokenSing(dataUser),
+      user: dataUser,
+    };
 
-  res.json(data);
+    res.json(data);
+  } catch (error) {
+    handleErrors.handleHttpError(res, error.message, 500);
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    req = matchedData(req);
+
+    /**--------------------------------------------------------------------
+     * | Invalidamos el 'select' en este caso para SI traer la password
+     * --------------------------------------------------------------------*/
+    const user = await usersModel
+      .findOne({ email: req.email })
+      .select("password name role email");
+
+    if (!user) return handleErrors.handleHttpError(res, "USER_NOT_EXIST", 404);
+
+    const hashPassword = user.password;
+    const check = await compare(req.password, hashPassword);
+
+    if (!check)
+      return handleErrors.handleHttpError(res, "PASSWORD_INVALID", 401);
+
+    user.set("password", undefined, { strict: false });
+    const data = {
+      token: await tokenSing(user),
+      user,
+    };
+
+    return res.json(data);
+  } catch (error) {
+    handleErrors.handleHttpError(res, error.message, 500);
+  }
 };
 
 module.exports = {
-    register,
-}
+  login,
+  register,
+};
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const controller = require("../controllers/auth.controller");
 
 const router = express.Router();
 
-router.post("/register", validators.validatorRegister, controller.register);
+router
+  .post("/register", validators.validatorRegister, controller.register)
+  .post("/login", validators.validatorLogin, controller.login)
 
 module.exports = router;
